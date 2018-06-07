9267cc9542de6439e30a0cbb988a8dc51e298bb1
Jude Beadle <info@jbeadle.de>
2018-06-07 22:29:36 +0200

    adding test for menu toggle

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -58,6 +58,8 @@ $(function() {
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
 
+        const menuIcon = $('.menu-icon-link');
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -72,6 +74,12 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('toggles visibility when clicked', function(){
+            menuIcon.click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            menuIcon.click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         })
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
